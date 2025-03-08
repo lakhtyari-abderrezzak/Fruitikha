c9a289424cc9f32b2f4144e57cec3a544addfb23
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-03-08 00:59:16 +0000

    Add content for show page for products

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,20 +2,47 @@
 
 
 @section('content')
-    <!-- breadcrumb-section -->
-    <div class="breadcrumb-section breadcrumb-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 offset-lg-2 text-center">
-                    <div class="breadcrumb-text">
-                        <p>See more Details</p>
-                        <h1></h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- end breadcrumb section -->
-    
-    <!-- end single product -->
+	<!-- breadcrumb-section -->
+	<div class="breadcrumb-section breadcrumb-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="breadcrumb-text">
+						<p>See more Details</p>
+						<h1>{{$product->name}}</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end breadcrumb section -->
+    <!-- single product -->
+	<div class="single-product mt-150 mb-150">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="single-product-img">
+						<img src="{{asset('storage/' . $product->img_url)}}" alt="">
+					</div>
+				</div>
+				<div class="col-md-7">
+					<div class="single-product-content">
+						<p class="single-product-pricing"><span>Per Kg</span> {{$product->price}}</p>
+						<p>{{$product->description}}</p>
+						<div class="single-product-form">
+							<a href="{{route('products.index', $product)}}" class="cart-btn">All {{$product->name}}</a>
+						</div>
+						<h4>Share:</h4>
+						<ul class="product-share">
+							<li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-google-plus-g"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end single product -->
 @endsection
